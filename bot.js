5a7155e18dd61ebdc0f7b5663349d4d5e9961690
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-05 14:27:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var adminprefix = "L";
+var adminprefix = "!";
 const developers = ["441554686399741962","323888904602124299"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'setg')) {
+  if (message.content.startsWith(adminprefix + 'setggggg')) {
     client.user.setGame(argresult);
       message.channel.send(`**تم تغيير حالتك الى** **${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
+  if (message.content.startsWith(adminprefix + 'setwgggg')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**تم تغيير الواتشنق الى** **${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
+  if (message.content.startsWith(adminprefix + 'setlggg')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**تم تغير اللستنق الى ** **${argresult}**`)
   } else 
@@ -25,11 +25,11 @@ client.on('message', message => {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
       message.channel.send(`**تم تغيير الستريمنق الى** **${argresult}**`)
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
+  if (message.content.startsWith(adminprefix + 'setnamgggge')) {
   client.user.setUsername(argresult).then
       message.channel.send(`**تم تغيير اسمك الى ** ..**${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'setava')) {
+if (message.content.startsWith(adminprefix + 'setggggava')) {
   client.user.setAvatar(argresult);
     message.channel.send(`**تم تغيير صورتك الى**:**${argresult}** `);
 }
